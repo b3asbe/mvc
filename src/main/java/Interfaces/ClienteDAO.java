89a82a6316dba4421872dcd45fc0eed0cc9caba8
b3asbe <168250840+b3asbe@users.noreply.github.com>
2025-06-10 15:25:54 -0500

    Delete src directory

--- src/main/java/Interfaces/ClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import Modelo.Cliente;
-
-public interface ClienteDAO {
-    public Cliente buscarPorDniRuc(String dniRuc);
-    public int agregarCliente(Cliente cliente); // Devuelve el ID generado o -1 si falla
-    // Puedes agregar más métodos como actualizar, listar, etc. si los necesitas para un CRUD de clientes.
-}
--- src/main/java/Interfaces/VendedorDAO.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-import Modelo.Vendedor;
-
-public interface VendedorDAO {
-    public Vendedor buscarPorNombre(String nombre); // O buscarPorId si es más conveniente
-    // Puedes agregar más métodos si necesitas un CRUD de vendedores.
-}
